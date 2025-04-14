c26ba2cbb86dc346053db7cc91d2d7cc2d2e6c32
yet92 <zyzy92187@gmail.com>
2025-04-14 13:54:14 +0300

    add ordering for Task model

--- todo/models.py
+++ todo/models.py
@@ -14,3 +14,6 @@ class Task(models.Model):
     deadline = models.DateTimeField(null=True)
     is_completed = models.BooleanField(default=False)
     tags = models.ManyToManyField(Tag, related_name="tasks")
+
+    class Meta:
+        ordering = ["is_completed", "-created_at"]
