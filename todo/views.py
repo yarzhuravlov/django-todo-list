067166f7578fc70182852631fcd954d44123e720
yet92 <zyzy92187@gmail.com>
2025-04-14 11:00:25 +0300

    add CBV TaskListView & templates/task_list.html template for TaskListView and templates/includes/task.html component

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.utils.timezone import now
+from django.views import generic
 
-# Create your views here.
+from todo.models import Task
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    queryset = Task.objects.prefetch_related("tags")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context["now"] = now()
+
+        return context
