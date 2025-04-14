c0e761dda6bbda3035865137068c1b40f2460fa1
yet92 <zyzy92187@gmail.com>
2025-04-14 12:00:59 +0300

    add __str__ method to Tag model

--- todo/models.py
+++ todo/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Task(models.Model):
