77090c25dd19b550fc456b37cb7591d98ced8d34
yet92 <zyzy92187@gmail.com>
2025-04-14 14:18:55 +0300

    add tasks/ prefix for actions with tasks. format todo/urls.py

--- todo/urls.py
+++ todo/urls.py
@@ -14,17 +14,49 @@ from todo.views import (
 
 app_name = "todo"
 urlpatterns = [
-    path("", TaskListView.as_view(), name="task-list"),
-    path("create/", TaskCreateView.as_view(), name="task-create"),
-    path("<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
-    path("<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
     path(
-        "<int:pk>/toggle_status/",
+        "",
+        TaskListView.as_view(),
+        name="task-list",
+    ),
+    path(
+        "tasks/create/",
+        TaskCreateView.as_view(),
+        name="task-create",
+    ),
+    path(
+        "tasks/<int:pk>/update/",
+        TaskUpdateView.as_view(),
+        name="task-update",
+    ),
+    path(
+        "tasks/<int:pk>/delete/",
+        TaskDeleteView.as_view(),
+        name="task-delete",
+    ),
+    path(
+        "tasks/<int:pk>/toggle_status/",
         ToggleTaskStatusView.as_view(),
         name="task-toggle-status",
     ),
-    path("tags/", TagListView.as_view(), name="tag-list"),
-    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
-    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
-    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
+    path(
+        "tags/",
+        TagListView.as_view(),
+        name="tag-list",
+    ),
+    path(
+        "tags/create/",
+        TagCreateView.as_view(),
+        name="tag-create",
+    ),
+    path(
+        "tags/<int:pk>/update/",
+        TagUpdateView.as_view(),
+        name="tag-update",
+    ),
+    path(
+        "tags/<int:pk>/delete/",
+        TagDeleteView.as_view(),
+        name="tag-delete",
+    ),
 ]
